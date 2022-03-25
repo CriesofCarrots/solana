7004bcf498bc7bde3581eb750526eccfe11d23de
Tyera Eulberg <tyera@solana.com>
2022-03-25 15:28:14 -0400

    more

--- ledger/src/blockstore.rs
+++ ledger/src/blockstore.rs
@@ -2315,6 +2315,10 @@ impl Blockstore {
     /// The first complete block that is available in the Blockstore ledger
     pub fn get_first_available_block(&self) -> Result<Slot> {
         let mut root_iterator = self.rooted_slot_iterator(self.lowest_slot())?;
+        warn!("{:?}", root_iterator.next());
+        warn!("{:?}", root_iterator.next());
+        
+        let mut root_iterator = self.rooted_slot_iterator(self.lowest_slot())?;
         warn!("{:?}", root_iterator.nth(0));
         warn!("{:?}", root_iterator.nth(1));
         // The block at root-index 0 cannot be complete, because it is missing its parent
