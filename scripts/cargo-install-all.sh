c7d2a5820b380b0a55549ea12c1d957ea4de448f
Tyera Eulberg <tyera@solana.com>
2023-12-12 19:44:39 -0700

    Pin spl-token-cli for cargo-install-all

--- scripts/cargo-install-all.sh
+++ scripts/cargo-install-all.sh
@@ -154,6 +154,7 @@ mkdir -p "$installDir/bin"
     "$cargo" $maybeRustVersion \
       --config 'patch.crates-io.ntapi.git="https://github.com/solana-labs/ntapi"' \
       --config 'patch.crates-io.ntapi.rev="97ede981a1777883ff86d142b75024b023f04fad"' \
+      --version v3.2.0 \
       install --locked spl-token-cli --root "$installDir"
   fi
 )
