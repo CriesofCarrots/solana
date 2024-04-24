c40c7d0037a6ccb0f4f9ea72562e7a7d42902ac4
Tyera Eulberg <tyera@anza.xyz>
2024-04-24 17:23:13 -0600

    comment out

--- runtime/benches/store_stake_accounts.rs
+++ runtime/benches/store_stake_accounts.rs
@@ -22,97 +22,97 @@ use {
 
 const NUM_ACCOUNTS: usize = 4096;
 
-fn populate_bank(
-    bank: &Bank,
-    num_accounts: usize,
-    starting_stake: u64,
-) -> Vec<(Pubkey, AccountSharedData)> {
-    let mut accounts = vec![];
-    let rent_exempt_reserve = bank.get_minimum_balance_for_rent_exemption(StakeStateV2::size_of());
-    let balance = rent_exempt_reserve + starting_stake;
-    let voter_pubkey = Pubkey::new_unique();
+// fn populate_bank(
+//     bank: &Bank,
+//     num_accounts: usize,
+//     starting_stake: u64,
+// ) -> Vec<(Pubkey, AccountSharedData)> {
+//     let mut accounts = vec![];
+//     let rent_exempt_reserve = bank.get_minimum_balance_for_rent_exemption(StakeStateV2::size_of());
+//     let balance = rent_exempt_reserve + starting_stake;
+//     let voter_pubkey = Pubkey::new_unique();
 
-    for _ in 0..num_accounts {
-        let stake_pubkey = Pubkey::new_unique();
-        let mut stake_account =
-            AccountSharedData::new(balance, StakeStateV2::size_of(), &stake::program::id());
-        let stake = Stake {
-            delegation: Delegation {
-                voter_pubkey,
-                stake: starting_stake,
-                ..Delegation::default()
-            },
-            credits_observed: 11,
-        };
-        stake_account
-            .set_state(&StakeStateV2::Stake(
-                Meta::default(),
-                stake,
-                StakeFlags::default(),
-            ))
-            .unwrap();
-        bank.store_account(&stake_pubkey, &stake_account);
-        accounts.push((stake_pubkey, stake_account));
-    }
+//     for _ in 0..num_accounts {
+//         let stake_pubkey = Pubkey::new_unique();
+//         let mut stake_account =
+//             AccountSharedData::new(balance, StakeStateV2::size_of(), &stake::program::id());
+//         let stake = Stake {
+//             delegation: Delegation {
+//                 voter_pubkey,
+//                 stake: starting_stake,
+//                 ..Delegation::default()
+//             },
+//             credits_observed: 11,
+//         };
+//         stake_account
+//             .set_state(&StakeStateV2::Stake(
+//                 Meta::default(),
+//                 stake,
+//                 StakeFlags::default(),
+//             ))
+//             .unwrap();
+//         bank.store_account(&stake_pubkey, &stake_account);
+//         accounts.push((stake_pubkey, stake_account));
+//     }
 
-    accounts
-}
+//     accounts
+// }
 
-#[bench]
-fn bench_store_stake_bulk(bencher: &mut Bencher) {
-    let (genesis_config, _) = create_genesis_config(10_000);
-    let bank = Bank::new_with_paths_for_benches(
-        &genesis_config,
-        vec![PathBuf::from("bench_store_stake_bulk")],
-    );
-    let accounts = populate_bank(&bank, NUM_ACCOUNTS, sol_to_lamports(1.0));
-    let ref_accounts: Vec<_> = accounts.iter().collect();
-    bencher.iter(|| {
-        bank.store_accounts((bank.slot(), &ref_accounts[..]));
-    });
-}
+// #[bench]
+// fn bench_store_stake_bulk(bencher: &mut Bencher) {
+//     let (genesis_config, _) = create_genesis_config(10_000);
+//     let bank = Bank::new_with_paths_for_benches(
+//         &genesis_config,
+//         vec![PathBuf::from("bench_store_stake_bulk")],
+//     );
+//     let accounts = populate_bank(&bank, NUM_ACCOUNTS, sol_to_lamports(1.0));
+//     let ref_accounts: Vec<_> = accounts.iter().collect();
+//     bencher.iter(|| {
+//         bank.store_accounts((bank.slot(), &ref_accounts[..]));
+//     });
+// }
 
-#[bench]
-fn bench_store_stake_load_and_store(bencher: &mut Bencher) {
-    let (genesis_config, _) = create_genesis_config(10_000);
-    let bank = Bank::new_with_paths_for_benches(
-        &genesis_config,
-        vec![PathBuf::from("bench_store_stake_load_and_store")],
-    );
-    let accounts = populate_bank(&bank, NUM_ACCOUNTS, sol_to_lamports(1.0));
-    bencher.iter(|| {
-        for (pubkey, _account) in &accounts {
-            let mut account = bank.get_account_with_fixed_root(&pubkey).unwrap();
-            let StakeStateV2::Stake(meta, mut stake, flags) = account.state().unwrap() else {
-                panic!();
-            };
-            stake.credits_observed += 10;
-            account
-                .set_state(&StakeStateV2::Stake(meta, stake, flags))
-                .unwrap();
-            bank.store_account(&pubkey, &account);
-        }
-    });
-}
+// #[bench]
+// fn bench_store_stake_load_and_store(bencher: &mut Bencher) {
+//     let (genesis_config, _) = create_genesis_config(10_000);
+//     let bank = Bank::new_with_paths_for_benches(
+//         &genesis_config,
+//         vec![PathBuf::from("bench_store_stake_load_and_store")],
+//     );
+//     let accounts = populate_bank(&bank, NUM_ACCOUNTS, sol_to_lamports(1.0));
+//     bencher.iter(|| {
+//         for (pubkey, _account) in &accounts {
+//             let mut account = bank.get_account_with_fixed_root(&pubkey).unwrap();
+//             let StakeStateV2::Stake(meta, mut stake, flags) = account.state().unwrap() else {
+//                 panic!();
+//             };
+//             stake.credits_observed += 10;
+//             account
+//                 .set_state(&StakeStateV2::Stake(meta, stake, flags))
+//                 .unwrap();
+//             bank.store_account(&pubkey, &account);
+//         }
+//     });
+// }
 
-#[bench]
-fn bench_store_stake_load_and_store_bulk(bencher: &mut Bencher) {
-    let (genesis_config, _) = create_genesis_config(10_000);
-    let bank = Bank::new_with_paths_for_benches(
-        &genesis_config,
-        vec![PathBuf::from("bench_store_stake_load_and_store_bulk")],
-    );
-    let mut accounts = populate_bank(&bank, NUM_ACCOUNTS, sol_to_lamports(1.0));
-    bencher.iter(|| {
-        for (pubkey, old_account) in accounts.iter_mut() {
-            let account = bank.get_account_with_fixed_root(&pubkey).unwrap();
-            let StakeStateV2::Stake(_meta, mut stake, _flags) = account.state().unwrap() else {
-                panic!();
-            };
-            stake.credits_observed += 10;
-            *old_account = account;
-        }
-        let ref_accounts: Vec<_> = accounts.iter().collect();
-        bank.store_accounts((bank.slot(), &ref_accounts[..]));
-    });
-}
+// #[bench]
+// fn bench_store_stake_load_and_store_bulk(bencher: &mut Bencher) {
+//     let (genesis_config, _) = create_genesis_config(10_000);
+//     let bank = Bank::new_with_paths_for_benches(
+//         &genesis_config,
+//         vec![PathBuf::from("bench_store_stake_load_and_store_bulk")],
+//     );
+//     let mut accounts = populate_bank(&bank, NUM_ACCOUNTS, sol_to_lamports(1.0));
+//     bencher.iter(|| {
+//         for (pubkey, old_account) in accounts.iter_mut() {
+//             let account = bank.get_account_with_fixed_root(&pubkey).unwrap();
+//             let StakeStateV2::Stake(_meta, mut stake, _flags) = account.state().unwrap() else {
+//                 panic!();
+//             };
+//             stake.credits_observed += 10;
+//             *old_account = account;
+//         }
+//         let ref_accounts: Vec<_> = accounts.iter().collect();
+//         bank.store_accounts((bank.slot(), &ref_accounts[..]));
+//     });
+// }
